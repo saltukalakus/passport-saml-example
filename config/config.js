9429ee6cca4a8bfce2391e510f2a47b3e5c4c5af
Saltuk Alakus <saltukalakus@gmail.com>
2020-11-25 21:43:01 +0300

    fix saml dependency

--- config/config.js
+++ config/config.js
@@ -18,29 +18,8 @@ iPZwgySTOJC7TFNJf+RXRlyc6S9/h1e21i36QYmI2fE1JjXca+4q6/A0sAZSogCv
 fZf5/joCRwD6
 -----END CERTIFICATE-----`;
 
-const tanverCert = `-----BEGIN CERTIFICATE-----
-MIIDDzCCAfegAwIBAgIJA1dNH6xpsTjMMA0GCSqGSIb3DQEBCwUAMCUxIzAhBgNV
-BAMTGnRhbnZlci1jdXN0b20uZXUuYXV0aDAuY29tMB4XDTE4MDcwNjExNTUxOVoX
-DTMyMDMxNDExNTUxOVowJTEjMCEGA1UEAxMadGFudmVyLWN1c3RvbS5ldS5hdXRo
-MC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCfLTV8D+p93Tm5
-9LuDLWWUIjluHw714Uf2CqqKsU4R7U4Um+PYbuolMbocobmRFROC28gIV4C8zh41
-6lWGFffXyNd9AZ0sPdlFkqF8ToFRSVqCLVVDlTXIO7R/TJhg73PUqhwWRWaVRf+3
-ZXhnLL/+QdSCLyPydzaFBHcA30cVYZAp1+ttWh/n++tbiUvd9Jo5Ca8ighStcXat
-jwgJGd9QteO7BXFfYgZ2ec1ITPCeQozdLO118UKm/WGWXkImP1qVzza0ECSnF9uq
-0fB6yBOlFWh1hdGP86Ww2fr4VMb0r1c/5TrXmPVgLCXYpTcWHZbllEvZPW7WtEXc
-j0TjwHDNAgMBAAGjQjBAMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFPlzIMUu
-oyhho8o2cwnjflCK9UmnMA4GA1UdDwEB/wQEAwIChDANBgkqhkiG9w0BAQsFAAOC
-AQEAk3XRxKyzrvbbnnG4ASyyV/MqTcjt6cebuxodIvyZjhs/2/QcZYbBk9gKyz6v
-VgBTf4TRsBBqrbEUX3o/uS7XjhsquyJJgCRBr1g6gH0Gt7OoyBkUcMfx6Fjv4cZM
-PNlso9rghkPPcJbxKrdUDIeILv8en7WegOxxYlOs7/6cP5iB3GMGlCQFrAq1JI+M
-gLqT+MOtyqq96GWNcByo0Ecvk9Yjf1xb7t7Dl85USfS1E9QOsMaiF4+pVybJ1Zwg
-vuDV4/8EI7n0lGqa8jj0vruKwgianKcIMDItSL7eE2v6VQkBPIeMibX/Z/DgnKMi
-OZsMYZj2etVQi0U8vz4T1rfc8g==
------END CERTIFICATE-----`;
 
-
-const saltukSaml = "https://saltuk-demo.auth0.com/samlp/WOVVA05PbWSk4xHUBZ2IXI9s1I6nN6cy?connection=Auth0Enterprise"
-const tanverSaml = "https://tanver-custom.eu.auth0.com/samlp/D7moGi1FQRfRwfTK2ra2KSA1ObLSzPJ7?connection=tanverhasannomanoutlook-waad";
+const saltukSaml = "https://saltuk-demo.auth0.com/samlp/WOVVA05PbWSk4xHUBZ2IXI9s1I6nN6cy"
 
 module.exports = {
   development: {
